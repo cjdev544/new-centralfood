c0d54c1caeec491c047a9f86bededdd214bd0d3f
Jefferson Campos <cjdev544@gmail.com>
2022-09-07 17:27:36 -0400

    total add products is ready

--- components/Complement/index.js
+++ components/Complement/index.js
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { FaPlus, FaCheck } from 'react-icons/fa'
 import style from './Complement.module.css'
 
-export default function Complement({ product }) {
+export default function Complement({ product, setComplements }) {
   const [isChecked, setIsChecked] = useState(false)
 
-  const handleClick = () => {
-    setIsChecked(!isChecked)
-  }
+  useEffect(() => {
+    if (isChecked) {
+      setComplements((products) => [...products, product])
+    } else {
+      setComplements((products) =>
+        products.filter((complement) => complement.id !== product.id)
+      )
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isChecked, product])
 
   return (
     <div
       className={isChecked ? style.itemBoxCheck : style.itemBox}
-      onClick={handleClick}
+      onClick={() => setIsChecked(!isChecked)}
     >
       <span>{product.nombre}</span>
       <div className={style.right}>
--- components/modals/ProductModal/index.js
+++ components/modals/ProductModal/index.js
@@ -1,5 +1,6 @@
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
+import { round } from 'mathjs'
 import { FaRegWindowClose, FaPlus, FaMinus } from 'react-icons/fa'
 
 import { getProductsComplements } from '../../../helpers/getProductsComplements'
@@ -7,7 +8,28 @@ import Complement from '../../Complement'
 import style from './ProductModal.module.css'
 
 export default function ProductModal({ products, product, setOpenModal }) {
+  const [counterProduct, setCounterProduct] = useState(1)
+  const [productCost, setProductCost] = useState(0)
+  const [complements, setComplements] = useState([])
+  const [complementsCost, setComplementsCost] = useState(0)
+  const [total, setTotal] = useState(0)
   const boxRef = useRef()
+
+  useEffect(() => {
+    setProductCost(round(product?.precio * counterProduct, 2))
+  }, [counterProduct, product])
+
+  useEffect(() => {
+    let total = 0
+    complements.forEach((complement) => {
+      total = round(Number(complement.precio) + total, 2)
+      setComplementsCost(total)
+    })
+  }, [complements])
+
+  useEffect(() => {
+    setTotal(round(productCost + complementsCost, 2))
+  }, [productCost, complementsCost])
 
   const isClicked = (e) => {
     if (!boxRef.current.contains(e.target)) {
@@ -20,6 +42,16 @@ export default function ProductModal({ products, product, setOpenModal }) {
     product
   )
 
+  const plusPlate = () => {
+    setCounterProduct(counterProduct + 1)
+  }
+
+  const minusPlate = () => {
+    if (counterProduct > 1) {
+      setCounterProduct(counterProduct - 1)
+    }
+  }
+
   return (
     <div className={style.modal} onClick={isClicked}>
       <div ref={boxRef} className={style.box}>
@@ -30,25 +62,41 @@ export default function ProductModal({ products, product, setOpenModal }) {
         <div className={style.left}>
           <p className={style.extra}>Extras</p>
           {extras.map((product) => (
-            <Complement key={product.id} product={product} />
+            <Complement
+              key={product.id}
+              product={product}
+              setComplements={setComplements}
+            />
           ))}
 
           {/* Drinks */}
           <p className={style.extra}>Bebidas</p>
           {drinks.map((product) => (
-            <Complement key={product.id} product={product} />
+            <Complement
+              key={product.id}
+              product={product}
+              setComplements={setComplements}
+            />
           ))}
 
           {/* Desserts */}
           <p className={style.extra}>Postres</p>
           {desserts.map((product) => (
-            <Complement key={product.id} product={product} />
+            <Complement
+              key={product.id}
+              product={product}
+              setComplements={setComplements}
+            />
           ))}
 
           {/* Beer */}
           <p className={style.extra}>Cervezas</p>
           {beer.map((product) => (
-            <Complement key={product.id} product={product} />
+            <Complement
+              key={product.id}
+              product={product}
+              setComplements={setComplements}
+            />
           ))}
         </div>
         <div className={style.right}>
@@ -64,16 +112,16 @@ export default function ProductModal({ products, product, setOpenModal }) {
           <p>{product.descripcion}</p>
           <span className={style.productPrice}>{product.precio}€</span>
           <div className={style.numberProducts}>
-            <div className={style.circle}>
+            <div className={style.circle} onClick={minusPlate}>
               <FaMinus />
             </div>
-            <span className={style.spanNumber}>1</span>
-            <div className={style.circle}>
+            <span className={style.spanNumber}>{counterProduct}</span>
+            <div className={style.circle} onClick={plusPlate}>
               <FaPlus />
             </div>
           </div>
           <div className={style.button}>
-            <button className='button'>Añadir por 5,50€</button>
+            <button className='button'>Añadir por {total}€</button>
           </div>
         </div>
       </div>
