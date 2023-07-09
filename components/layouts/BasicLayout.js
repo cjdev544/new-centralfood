6ea7e0570710a7c1cbf8c9dc508dc0991b867261
cjdev544 <cjdev544@gmail.com>
2023-07-09 18:58:47 -0400

    isOpen manual close

--- components/layouts/BasicLayout.js
+++ components/layouts/BasicLayout.js
@@ -34,7 +34,7 @@ export default function BasicLayout({ children }) {
         pauseOnHover
       />
       <div id='up' />
-      {false === false && (
+      {isOpen === false && (
         <div className={style.isOpen}>
           <span>El restaurante se encuentra cerrado en estos momentos</span>
         </div>
