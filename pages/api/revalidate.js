8e299cb0868492487fd48d2091c6f8ae0a0d6f88
CjDev544 <cjdev544@gmail.com>
2024-01-19 09:55:39 -0400

    add .env.example, and change revalidate time

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
               msg: 'No se pudo actualizar la página, intente mas tarde',
             })
           })
-      }, 20000)
+      }, 8000)
     }
   )
 }
